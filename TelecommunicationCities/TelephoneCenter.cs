73897aa082b51a22ceab2a73e4211fbbb9352608
Romain Neil <tutosfaciles48@gmail.com>
2021-05-29 15:07:52 +0200

    feat(TelephoneCenter): add basics

---
+++ TelecommunicationCities/TelephoneCenter.cs
@@ -0,0 +1,26 @@
+﻿namespace TelecommunicationCities
+{
+	public class TelephoneCenter
+	{
+
+		public enum BuildingType
+		{
+			Copper,
+			Fiber
+		}
+
+		public int GetPrice(BuildingType buildingType)
+		{
+			switch (buildingType)
+			{
+				case BuildingType.Copper:
+					return 2000;
+				case BuildingType.Fiber:
+					return 1000;
+				default:
+					return 0;
+			}
+		}
+		
+	}
+}

193d407cc69e76aa1d47101c64c3d0473e96692a
Romain Neil <tutosfaciles48@gmail.com>
2021-05-29 15:07:08 +0200

    feat(Antenna): add base class

---
+++ TelecommunicationCities/Antenna.cs
@@ -0,0 +1,27 @@
+﻿namespace TelecommunicationCities
+{
+	public class Antenna
+	{
+		public enum Generation
+		{
+			G3,
+			G4,
+			G5,
+		}
+
+		public int GetPrice(Generation generation)
+		{
+			switch (generation)
+			{
+				case Generation.G3:
+					return 5000;
+				case Generation.G4:
+					return 10000;
+				case Generation.G5:
+					return 20000;
+				default:
+					return 0;
+			}
+		}
+	}
+}

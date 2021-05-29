aa830cb588ee501150d761641b83effc6d3faec9
Romain Neil <tutosfaciles48@gmail.com>
2021-05-29 15:06:43 +0200

    feat(Main): add mod basic

---
+++ TelecommunicationCities/Main.cs
@@ -0,0 +1,19 @@
+﻿using ICities;
+
+namespace TelecommunicationCities
+{
+	#region Mod definition
+	public class TelcoCities : IUserMod
+	{
+		public string Name
+		{
+			get { return "Telecommunication Cities"; }
+		}
+
+		public string Description
+		{
+			get { return "Mod to add some telecommunications (xdsl, fiber, telephonic/fiber center) and some mobile stuff (3g, 4G and 5G !) "; }
+		}
+	}
+	#endregion
+}
